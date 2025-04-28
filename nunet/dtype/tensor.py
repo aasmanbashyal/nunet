1f7b1abf51b1afc19bcf9dc93822b17a07a36574
bashyalaasman <bashyalaasman@gmail.com>
2025-04-28 16:09:37 +0545

    Formatting: pep6

--- nunet/dtype/tensor.py
+++ nunet/dtype/tensor.py
@@ -1,17 +1,18 @@
 import numpy as np
 
+
 class Tensor(np.ndarray):
 
-    def __new__(cls, shape, init_method = "rand"):
-        base_array = cls.init_weights(shape=shape, init_method= init_method)
+    def __new__(cls, shape, init_method="rand"):
+        base_array = cls.init_weights(shape=shape, init_method=init_method)
         tensor = base_array.view(cls)
         tensor._gradient = np.zeros(shape)
         return tensor
-    
+
     @property
     def gradient(self):
         return self._gradient
-    
+
     @gradient.setter
     def gradient(self, value):
         self._gradient = value
@@ -20,12 +21,11 @@ class Tensor(np.ndarray):
     def init_weights(shape, init_method):
         if init_method == 'ones':
             return np.ones(shape)
-        
+
         elif init_method == 'rand':
             return np.random.rand(shape)
-        
-        elif init_method == 'normal':
-            return np.random.normal(size = shape)
-        else:
-            return np.zeros(shape) 
 
+        elif init_method == 'normal':
+            return np.random.normal(size=shape)
+        else:
+            return np.zeros(shape)
