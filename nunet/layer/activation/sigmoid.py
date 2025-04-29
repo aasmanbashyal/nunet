9a4a83f6bb699bafaf004c7615e501836170d3d0
bashyalaasman <bashyalaasman@gmail.com>
2025-04-29 16:04:39 +0545

    Fix: add negative mask too

--- nunet/layer/activation/sigmoid.py
+++ nunet/layer/activation/sigmoid.py
@@ -5,16 +5,20 @@ from nunet.layer import Module
 class Sigmoid(Module):
     def __init__(self):
         self._last_output = None
-        print("Sigmoid module created.")
 
     def forward(self, input_data):
         input_array = np.array(input_data)
-
-        output = np.where(
-            input_array >= 0,
-            1 / (1 + np.exp(-input_array)),
-            np.exp(input_array) / (1 + np.exp(input_array))
-        )
+        
+        output = np.zeros_like(input_array)
+        
+        pos_mask = input_array >= 0
+        neg_mask = ~pos_mask
+        
+        output[pos_mask] = 1.0 / (1.0 + np.exp(-input_array[pos_mask]))
+        
+        neg_exp = np.exp(input_array[neg_mask])
+        output[neg_mask] = neg_exp / (1.0 + neg_exp)
+        
         self._last_output = output
         return output
 
